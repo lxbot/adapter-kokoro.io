6c4558d731202f54b3510f9059b547ac8e64695b
mohemohe <mohemohe@ghippos.net>
2020-01-14 23:59:39 +0900

    tabun OK

--- adapter.go
+++ adapter.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
 )
 
 var ch *chan map[string]interface{}
@@ -18,7 +22,36 @@ func Boot(c *chan map[string]interface{}) {
 }
 
 func Send(msg map[string]interface{}) {
+	channelId := msg["room"].(map[string]interface{})["id"].(string)
+	message := msg["message"].(map[string]interface{})["text"].(string)
+	_ = send(channelId, message)
+}
 
+func Reply(msg map[string]interface{}) {
+
+}
+
+func send(channelId string, msg string) error {
+	u := "https://kokoro.io/api/v1/bot/channels/"+channelId+"/messages"
+
+	v := url.Values{}
+	v.Set("message", msg)
+
+	req, err := http.NewRequest("POST", u, strings.NewReader(v.Encode()))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", token)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return err
 }
 
 func listen() {
@@ -34,19 +67,33 @@ func get(c echo.Context) error {
 
 func post(c echo.Context) error {
 	m := map[string]interface{}{}
-	if err := c.Bind(m); err != nil {
+	if err := c.Bind(&m); err != nil {
 		return c.NoContent(406)
 	}
 
-	_ = c.NoContent(201)
+	resErr := c.NoContent(202)
 
-	println(m)
+	fmt.Println(m)
 
 	*ch <- map[string]interface{}{
-		"test": 1,
+		"user": map[string]interface{}{
+			"id": m["profile"].(map[string]interface{})["screen_name"],
+			"name": m["display_name"],
+		},
+		"room": map[string]interface{}{
+			"id": m["channel"].(map[string]interface{})["id"],
+			"name": m["channel"].(map[string]interface{})["channel_name"],
+			"description": m["channel"].(map[string]interface{})["description"],
+		},
+		"message": map[string]interface{}{
+			"id": m["id"],
+			"text": m["plaintext_content"],
+			"attachments": nil,
+		},
+		"raw": m,
 	}
 
-	return nil
+	return resErr
 }
 
 func authorize(next echo.HandlerFunc) echo.HandlerFunc {
